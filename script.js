879b823ad9c2fb640f1fbc99758ef7bf0a85d465
mendesjar <ga0301@outlook.com>
2024-07-23 09:44:14 -0300

    feat: path image static

--- script.js
+++ script.js
@@ -14,12 +14,13 @@ window.onload = function () {
 
   const renderDados = async (pokemon) => {
     const pokemonEncontrado = await buscarPokemons(pokemon);
+    const pathImage =
+      pokemonEncontrado["sprites"]["versions"]["generation-v"]["black-white"];
+    const imageAnimated = pathImage["animated"]["front_default"];
+    const imageStatic = pathImage["front_default"];
     idPokemon.innerHTML = pokemonEncontrado.id;
     nomePokemon.innerHTML = pokemonEncontrado?.name?.toUpperCase();
-    imgPokemon.src =
-      pokemonEncontrado["sprites"]["versions"]["generation-v"]["black-white"][
-        "animated"
-      ]["front_default"];
+    imgPokemon.src = imageAnimated || imageStatic;
   };
 
   formPokemon.addEventListener("submit", (e) => {
